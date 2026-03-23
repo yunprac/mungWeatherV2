d264321f8fd4527e8e3b9001aeba8467d9bf6688
yunprac <leeyun002007@gmail.com>
2026-03-23 17:57:46 +0900

    실시간 위치 조회로 변경

--- app/src/main/java/com/example/myapp/data/location/LocationProvider.kt
+++ app/src/main/java/com/example/myapp/data/location/LocationProvider.kt
@@ -1,11 +1,12 @@
 package com.example.myapp.data.location
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -19,18 +20,47 @@ class LocationProvider(
     @SuppressLint("MissingPermission")
     suspend fun getCurrentLocation(): Location =
         suspendCancellableCoroutine { cont ->
-            fusedLocationClient.lastLocation
+            val cancellationTokenSource = CancellationTokenSource()
+
+            fusedLocationClient.getCurrentLocation(
+                Priority.PRIORITY_HIGH_ACCURACY,
+                cancellationTokenSource.token
+            )
                 .addOnSuccessListener { location ->
                     if (location != null) {
                         cont.resume(location)
                     } else {
-                        cont.resumeWithException(
-                            IllegalStateException("현재 위치를 가져올 수 없습니다.")
-                        )
+                        fusedLocationClient.lastLocation
+                            .addOnSuccessListener { lastLocation ->
+                                if (lastLocation != null) {
+                                    cont.resume(lastLocation)
+                                } else {
+                                    cont.resumeWithException(
+                                        IllegalStateException("현재 위치를 가져올 수 없습니다. 위치 서비스를 확인해 주세요.")
+                                    )
+                                }
+                            }
+                            .addOnFailureListener { e ->
+                                cont.resumeWithException(e)
+                            }
                     }
                 }
                 .addOnFailureListener { e ->
-                    cont.resumeWithException(e)
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { lastLocation ->
+                            if (lastLocation != null) {
+                                cont.resume(lastLocation)
+                            } else {
+                                cont.resumeWithException(e)
+                            }
+                        }
+                        .addOnFailureListener { fallbackError ->
+                            cont.resumeWithException(fallbackError)
+                        }
                 }
+
+            cont.invokeOnCancellation {
+                cancellationTokenSource.cancel()
+            }
         }
 }
