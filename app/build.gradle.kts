3042660eec33a08f97b8ed8c8b44c3287f8b65bd
yunprac <leeyun002007@gmail.com>
2026-03-24 16:41:46 +0900

    배포 준비

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.example.myapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
