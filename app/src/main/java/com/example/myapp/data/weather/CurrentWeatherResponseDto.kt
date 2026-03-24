529e6833ee56577caed1073760c15329b5153db5
yunprac <leeyun002007@gmail.com>
2026-03-24 15:33:13 +0900

    배포 준비

--- app/src/main/java/com/example/myapp/data/weather/CurrentWeatherResponseDto.kt
+++ app/src/main/java/com/example/myapp/data/weather/CurrentWeatherResponseDto.kt
@@ -1,16 +1,25 @@
 package com.example.myapp.data.weather
 
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
 data class CurrentWeatherResponseDto(
+    @SerializedName("weather")
     val weather: List<WeatherDto>,
     val main: MainDto
 )
 
+@Keep
 data class WeatherDto(
+    @SerializedName("id")
     val id: Int,
     val main: String,
     val description: String
 )
 
+@Keep
 data class MainDto(
+    @SerializedName("temp")
     val temp: Double
 )
